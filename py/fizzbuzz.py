b4ed58ac15674268736f93a8339ee7cc3836a87b
mindcat <maw4678@rit.edu>
2022-12-01 23:51:08 -0500

    added one line solution and made default conditional sol more standard, at the cost of brevity

--- py/fizzbuzz.py
+++ py/fizzbuzz.py
@@ -1,7 +1,9 @@
 for num in range(1, 100):
-    print(num, '...', end='')
-    if num % 3 == 0:
-        print('fizz', end='')
-    if num % 5 == 0:
-        print('buzz', end='')
-    print('\n', end='')
+    if num % 3 == 0 and num % 5 == 0:
+        print('fizzbuzz')
+    elif num % 3 == 0:
+        print('fizz')
+    elif num % 5 == 0:
+        print('buzz')
+    else:
+        print(num)
---
+++ py/fizzbuzz_1l.py
@@ -0,0 +1,1 @@
+for i in range(1, 100): print('fizz'*(i%3==0)+'buzz'*(i%5==0) or str(i))
